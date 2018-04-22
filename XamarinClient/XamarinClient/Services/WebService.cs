3f698f36276b1cbb48ad5d39a0d0ca6ebbc647f3
EmilienLeroy <emilien1005@hotmail.fr>
2018-04-22 16:25:23 +0200

    add webservice async get all devises

---
+++ XamarinClient/XamarinClient/Services/WebService.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using XamarinClient.Models;
+
+namespace XamarinClient.Services
+{
+    class WebService
+    {
+        private HttpClient _httpclient;
+
+        public WebService()
+        {
+            this._httpclient = new HttpClient();
+            this._httpclient.BaseAddress = new Uri("http://192.168.1.180:50088/api/");
+        }
+
+        public async Task<List<Devise>> getAllDevisesAsync()
+        {
+            List<Devise> devises = null;
+            HttpResponseMessage response = await _httpclient.GetAsync("Devise");
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                devises = JsonConvert.DeserializeObject<List<Devise>>(content);
+            }
+            return devises;
+        }
+    }
+}
+     
